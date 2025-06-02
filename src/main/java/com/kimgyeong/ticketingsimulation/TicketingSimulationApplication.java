fd7e50634bb980e3038460389514d36dd4b1d7cc
KimGyeong <taekwondo714@naver.com>
2025-06-02 18:16:51 +0900

    Initial Commit
    - 프로젝트 세팅

---
+++ src/main/java/com/kimgyeong/ticketingsimulation/TicketingSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.kimgyeong.ticketingsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimgyeong/ticketingsimulation/TicketingSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimgyeong.ticketingsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
